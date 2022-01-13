35ba0433a902e72b38e0aad4bfdabbca120fa62c
sjovanovic974 <sjovanovic974@gmail.com>
2022-01-13 10:38:51 +0100

    Added Cross Origin global configuration. Closes #9

--- src/main/java/sasa/jovanovic/musicshop/repos/ProductCategoryRepository.java
+++ src/main/java/sasa/jovanovic/musicshop/repos/ProductCategoryRepository.java
@@ -2,11 +2,9 @@ package sasa.jovanovic.musicshop.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import sasa.jovanovic.musicshop.models.ProductCategory;
 
 @Repository
-@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
 }
--- src/main/java/sasa/jovanovic/musicshop/repos/ProductRepository.java
+++ src/main/java/sasa/jovanovic/musicshop/repos/ProductRepository.java
@@ -4,11 +4,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import sasa.jovanovic.musicshop.models.Product;
 
 @Repository
-@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(Long id, Pageable page);
