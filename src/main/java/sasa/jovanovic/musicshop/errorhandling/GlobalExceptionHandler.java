edc78ef73b71f2a316817398b83b25fb794f9b8e
sjovanovic974 <sjovanovic974@gmail.com>
2022-02-13 11:00:18 +0100

    Modified date format in ErrorResponse

--- src/main/java/sasa/jovanovic/musicshop/errorhandling/GlobalExceptionHandler.java
+++ src/main/java/sasa/jovanovic/musicshop/errorhandling/GlobalExceptionHandler.java
@@ -6,31 +6,38 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = {NotFoundException.class})
-    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException exception){
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException exception) {
 
         ErrorResponse error = new ErrorResponse();
 
         error.setStatus(HttpStatus.NOT_FOUND.value());
         error.setMessage(exception.getMessage());
-        error.setTimeStamp(LocalDateTime.now().toString());
+        error.setTimeStamp(formatDateTime((LocalDateTime.now())));
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = {Exception.class})
-    public ResponseEntity<ErrorResponse> handleException(Exception exception){
+    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
 
         ErrorResponse error = new ErrorResponse();
 
         error.setStatus(HttpStatus.BAD_REQUEST.value());
         error.setMessage(exception.getMessage());
-        error.setTimeStamp(LocalDateTime.now().toString());
+        error.setTimeStamp(formatDateTime((LocalDateTime.now())));
 
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    private String formatDateTime(LocalDateTime dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        return dateTime.format(formatter);
     }
 }
